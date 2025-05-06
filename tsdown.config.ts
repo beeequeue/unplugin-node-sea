54bc263f4975a83debd88ae60dfc4ebac1d949d3
Adam <adam@haglund.dev>
2025-05-06 12:53:26 +0200

    remove unused stuff from tsdown config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,17 +1,8 @@
-import process from "node:process"
-
 import { defineConfig } from "tsdown"
 
 export default defineConfig({
   entry: ["src/plugin.ts"],
   outDir: "dist",
-
-  env: {
-    NODE_ENV: process.env.NODE_ENV ?? "production",
-    DEV: process.env.NODE_ENV === "development",
-    PROD: process.env.NODE_ENV === "production",
-    TEST: false,
-  },
 
   platform: "node",
   format: "esm",
