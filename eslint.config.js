c1fb81639cd9dc351be51a1bd82cae8fc9939ab4
Adam <adam@haglund.dev>
2025-06-23 17:43:04 +0200

    update linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,26 @@
 import antfu from "@antfu/eslint-config"
 
+const sortImports = {
+  "perfectionist/sort-imports": [
+    "error",
+    {
+      type: "natural",
+      internalPattern: ["^@/", "^~/", "^#[a-zA-Z0-9-]+/"],
+      newlinesBetween: "always",
+      groups: [
+        ["builtin", "builtin-type"],
+        ["external", "external-type"],
+        ["internal", "internal-type"],
+        ["parent", "parent-type"],
+        ["sibling", "sibling-type"],
+        ["index", "index-type"],
+        "object",
+        "unknown",
+      ],
+    },
+  ],
+}
+
 export default antfu({
   ignores: ["**/*.json"],
   markdown: false,
@@ -25,33 +46,16 @@ export default antfu({
 
     overrides: {
       "no-console": "off",
-      "ts/no-use-before-define": "off",
+      "antfu/no-top-level-await": "off",
+      "import/consistent-type-specifier-style": ["error", "prefer-top-level"],
+      "node/prefer-global/process": "off",
       "ts/consistent-type-definitions": "off",
-      "ts/consistent-type-imports": ["error", { fixStyle: "inline-type-imports" }],
       "ts/no-unsafe-argument": "off",
       "ts/no-unsafe-assignment": "off",
-      "node/prefer-global/process": "off",
-      "antfu/no-top-level-await": "off",
-      "import/consistent-type-specifier-style": "off",
+      "ts/no-use-before-define": "off",
+      "unused-imports/no-unused-vars": "off",
 
-      "perfectionist/sort-imports": [
-        "error",
-        {
-          type: "natural",
-          internalPattern: ["^@/", "^~/", "^#[a-zA-Z0-9-]+/"],
-          newlinesBetween: "always",
-          groups: [
-            ["builtin", "builtin-type"],
-            ["external", "external-type"],
-            ["internal", "internal-type"],
-            ["parent", "parent-type"],
-            ["sibling", "sibling-type"],
-            ["index", "index-type"],
-            "object",
-            "unknown",
-          ],
-        },
-      ],
+      ...sortImports,
     },
   },
 })
